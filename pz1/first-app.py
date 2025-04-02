4b0700018e945dfb904ca067bb449d9ea7110215
IntAtoi <m.gnatusiv5@gmail.com>
2025-04-02 09:32:49 +0200

    Add files via upload

---
+++ pz1/first-app.py
@@ -0,0 +1,24 @@
+
+import json
+
+import nbu
+import requests
+nbu.response = requests.get("https://bank.gov.ua/NBU_Exchange/exchange_site?start=20250324&end=20250224&valcode=usd&sort=exchangedate&order=desc&json")
+#print(nbu_respoonse)
+#print(nbu_respoonse.content)
+
+converted_response= json.loads(nbu.content)
+#print(converted_response)
+
+exchange_dates = []
+exchange_rates = []
+
+for item in converted_response:
+    exchange_dates.append(item['exchangedate'])
+    exchange_rates.append(item['rate'])
+    print(exchange_rates)
+    print(exchange_dates)
+
+    import matplotlib.pyplot as plt
+    plt.plot(exchange_dates, exchange_rates)
+    plt.show()
